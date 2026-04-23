ee305cd74af55cf753c271766100a211a62ef00a
Clément XaT <xatrixblue@gmail.com>
2026-04-23 21:28:18 +0200

    test: cover topic block grouping

--- feature/topic/src/test/java/fr/forumhfr/redface2/feature/topic/TopicHtmlRendererTest.kt
+++ feature/topic/src/test/java/fr/forumhfr/redface2/feature/topic/TopicHtmlRendererTest.kt
@@ -7,6 +7,20 @@ import org.junit.Test
 
 class TopicHtmlRendererTest {
     @Test
+    fun parseTopicBlocks_groupsContiguousInlineNodesIntoSingleParagraph() {
+        val blocks = parseTopicBlocks("""<a href="https://forum.hardware.fr/">link</a> ----> <b>next</b>""")
+
+        assertEquals(1, blocks.size)
+        val paragraph = blocks.single() as TopicContentBlock.Paragraph
+        val rendered = renderInlineTopicHtml(
+            html = paragraph.html,
+            linkStyles = TextLinkStyles(style = SpanStyle()),
+        )
+
+        assertEquals("link ----> next", rendered.text)
+    }
+
+    @Test
     fun renderInlineTopicHtml_preservesSeparatorsAroundInlineNodes() {
         val rendered = renderInlineTopicHtml(
             html = """<a href="https://forum.hardware.fr/">link</a> ----> <b>next</b>""",
